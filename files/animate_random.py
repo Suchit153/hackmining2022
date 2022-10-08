127a2d4a207862af1047b42263f3b7a927f3b8f7
Timur Kuzu <timur.kuzu@rwth-aachen.de>
2022-10-08 12:21:42 +0000

    Upload New File

---
+++ files/animate_random.py
@@ -0,0 +1,24 @@
+import random
+from itertools import count
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+plt.style.use('fivethirtyeight')
+
+x_values = []
+y_values = []
+ 
+index = count()
+
+def animate(i):
+    x_values.append(next(index))
+    y_values.append(random.randint(0, 10))
+    plt.cla()
+    plt.plot(x_values, y_values)
+
+
+ani = FuncAnimation(plt.gcf(), animate, 1000)
+
+
+plt.tight_layout()
+plt.show()
